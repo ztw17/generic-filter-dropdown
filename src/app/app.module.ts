5235d0dd1d7cb95bcbc636cd02f5c551456c6ad5
Zach Weber <weberzt@gmail.com>
2023-05-18 11:33:31 -0600

    Add new component; basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterDropdownComponent } from './generic-filter/filter-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterDropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/generic-filter/filter-dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-dropdown',
+  templateUrl: './filter-dropdown.component.html',
+  styleUrls: ['./filter-dropdown.component.scss'],
+})
+
+export class FilterDropdownComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+}
